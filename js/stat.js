26d719d1a83583b10332f862c35a11cf1945df45
Aleks <alekssson@gmail.com>
2018-06-09 19:56:54 +0300

    Исправила цвет гистограммы

--- js/stat.js
+++ js/stat.js
@@ -10,7 +10,7 @@ var FONT_GAP = 15;
 var BAR_WIDTH = 40;
 var BAR_HEIGHT = 150;
 
-var renderCloud = function(ctx, x, y, width, height, gap, margin, color) {
+var renderCloud = function (ctx, x, y, width, height, gap, margin, color) {
   ctx.fillStyle = color;
   ctx.beginPath();
   ctx.moveTo(x + margin, y);
@@ -24,46 +24,44 @@ var renderCloud = function(ctx, x, y, width, height, gap, margin, color) {
   ctx.quadraticCurveTo(x + gap, y + gap, x + margin, y);
   ctx.closePath();
   ctx.fill();
-}
+};
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   var maxElement = arr[0];
-  
-  for(var i=0; i<arr.length; i++) {
-    if(arr[i] > maxElement) {
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
       maxElement = arr[i];
     }
   }
   return maxElement;
-}
+};
 
-window.renderStatistics = function(ctx, names, times) {
-  renderCloud(ctx, CLOUD_X+GAP, CLOUD_Y+GAP, CLOUD_WIDTH, CLOUD_HEIGHT, GAP, MARGIN, 'rgba(0, 0, 0, 0.7)');
+window.renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, CLOUD_WIDTH, CLOUD_HEIGHT, GAP, MARGIN, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, CLOUD_WIDTH, CLOUD_HEIGHT, GAP, MARGIN, '#fff');
-  
+
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
   ctx.textBaseline = 'hanging';
-  ctx.fillText('Ура, Вы победили!', CLOUD_X + MARGIN*2, CLOUD_Y + FONT_GAP);
-  ctx.fillText('Список результатов', CLOUD_X + MARGIN*2, CLOUD_Y + FONT_GAP*3);
-  
+  ctx.fillText('Ура, Вы победили!', CLOUD_X + MARGIN * 2, CLOUD_Y + FONT_GAP);
+  ctx.fillText('Список результатов', CLOUD_X + MARGIN * 2, CLOUD_Y + FONT_GAP * 3);
+
   var maxTime = getMaxElement(times);
-  
-  for(var i=0; i<names.length; i++) {
-    //var number = Math.floor(Math.random()*101);
-    //не знаю, как соединить рандомное число со знаком процента, чтобы сработал цвет
-    
-    ctx.fillStyle = 'hsl(240, 50%, 50%)';
-    
-    if(names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';   
+
+  for (var i = 0; i < names.length; i++) {
+    var number = Math.floor(Math.random() * 100);
+
+    ctx.fillStyle = 'hsl(240, ' + number + '%, 50%)';
+
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     }
-    
-    ctx.fillRect(CLOUD_X + MARGIN + (BAR_WIDTH + MARGIN)*i, CLOUD_Y + FONT_GAP*5 + (BAR_HEIGHT - (BAR_HEIGHT*times[i])/maxTime), BAR_WIDTH, (BAR_HEIGHT*times[i])/maxTime);
-    
+    var startPointX = CLOUD_X + MARGIN + (BAR_WIDTH + MARGIN) * i;
+    var startPointY = CLOUD_Y + FONT_GAP * 5 + (BAR_HEIGHT - (BAR_HEIGHT * times[i]) / maxTime);
+    ctx.fillRect(startPointX, startPointY, BAR_WIDTH, (BAR_HEIGHT * times[i]) / maxTime);
+
     ctx.fillStyle = '#000';
     ctx.textBaseline = 'hanging';
-    ctx.fillText(names[i], CLOUD_X + MARGIN + (BAR_WIDTH + MARGIN)*i, (CLOUD_Y + CLOUD_HEIGHT) - FONT_GAP*2);
-  } 
-}
- 
+    ctx.fillText(names[i], CLOUD_X + MARGIN + (BAR_WIDTH + MARGIN) * i, (CLOUD_Y + CLOUD_HEIGHT) - FONT_GAP * 2);
+  }
+};
